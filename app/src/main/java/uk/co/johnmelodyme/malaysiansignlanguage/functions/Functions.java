d5b65f2fe3e2dc1b3d5dff4a1e265db6822eeb14
John Melody Me <Johnmelodyme@yandex.com>
2021-07-18 23:53:57 +0800

    #2 Adde User Permission Component ? request ? inspect

--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/functions/Functions.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/functions/Functions.java
@@ -1,10 +1,19 @@
 package uk.co.johnmelodyme.malaysiansignlanguage.functions;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.constant.Constants;
 import uk.co.johnmelodyme.malaysiansignlanguage.constant.LogLevel;
@@ -59,6 +68,42 @@ public class Functions extends Constants
             }
         }
 
+    }
+
+    /**
+     * @param context Current instance of the application are required for
+     *                getting user permission either accepted or denied.
+     */
+    public static void get_user_permission(@NonNull Context context)
+    {
+        LogLevel permission = LogLevel.DEBUG;
+
+        Dexter.withContext(context).withPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CALL_PHONE
+        ).withListener(new MultiplePermissionsListener()
+        {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport)
+            {
+                log_output("get_user_permission/1", 0, permission);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> l,
+                                                           PermissionToken pt)
+            {
+
+                if (pt != null)
+                {
+                    log_output("get_user_permission/1", 0, permission);
+                }
+
+            }
+        }).onSameThread().check();
     }
 
     /**
