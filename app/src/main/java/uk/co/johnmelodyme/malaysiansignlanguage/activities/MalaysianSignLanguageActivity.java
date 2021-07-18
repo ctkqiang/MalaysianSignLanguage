4b6ab67a405b0e15e0cf46bc34f062a5b5748d12
John Melody Me <Johnmelodyme@yandex.com>
2021-07-18 23:52:42 +0800

    #1 Added Custom Action Bar

--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/MalaysianSignLanguageActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/MalaysianSignLanguageActivity.java
@@ -5,14 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.R;
+import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
+
+
+/**
+ * @author John Melody Me <johnmelody@dingtalk.com>
+ * <p>
+ * This file is part of Malaysian Sign Language.
+ * <p>
+ * Malaysian Sign Language is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Malaysian Sign Language is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Malaysian Sign Language.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 public class MalaysianSignLanguageActivity extends AppCompatActivity
 {
+    public void render_user_components(Bundle bundle)
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Render Action Bar */
+        ComponentFunctions.render_action_bar(this, savedInstanceState);
+
+        /* Render Bottom System Bar */
+        ComponentFunctions.render_bottom_navigation(this);
     }
 }
--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/functions/ComponentFunctions.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/functions/ComponentFunctions.java
@@ -1,11 +1,14 @@
 package uk.co.johnmelodyme.malaysiansignlanguage.functions;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import uk.co.johnmelodyme.malaysiansignlanguage.R;
 import uk.co.johnmelodyme.malaysiansignlanguage.constant.LogLevel;
 
 /**
@@ -35,5 +38,33 @@ public class ComponentFunctions extends Functions
 
         ActionBar actionBar = activity.getSupportActionBar();
         actionBar.hide();
+    }
+
+    /**
+     * @param activity The User Current Instances of the Activity
+     * @param bundle   The Instance of the class rendered
+     */
+    public static void render_action_bar(AppCompatActivity activity, Bundle bundle)
+    {
+        log_output("render_action_bar/2", 0, LogLevel.DEBUG);
+
+        ActionBar actionbar = activity.getSupportActionBar();
+
+        if (actionbar != null)
+        {
+            actionbar.setBackgroundDrawable(new ColorDrawable(activity.getResources().getColor(R.color.main)));
+            activity.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            activity.getSupportActionBar().setCustomView(R.layout.action_bar);
+        }
+    }
+
+    /**
+     * @param activity Get Current UI Instances and renders color to bottom Navigation
+     */
+    public static void render_bottom_navigation(AppCompatActivity activity)
+    {
+        log_output("render_bottom_navigation/1", 0, LogLevel.DEBUG);
+
+        activity.getWindow().setNavigationBarColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.main));
     }
 }
