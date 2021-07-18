5574725522e8e3e6a99abc18941360b866d69c94
John Melody Me <Johnmelodyme@yandex.com>
2021-07-18 23:17:50 +0800

    #1 Added Splash Screen and readjusted MainActivity

--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/SplashActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/SplashActivity.java
@@ -3,8 +3,30 @@ package uk.co.johnmelodyme.malaysiansignlanguage.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.R;
+import uk.co.johnmelodyme.malaysiansignlanguage.constant.Constants;
+import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
+
+/**
+ * @author John Melody Me <johnmelody@dingtalk.com>
+ * <p>
+ * This file is part of Malaysian Sign Language.
+ * <p>
+ * Malaysian Sign Language is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Malaysian Sign Language is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Malaysian Sign Language.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 public class SplashActivity extends AppCompatActivity
 {
@@ -14,5 +36,29 @@ public class SplashActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /* Hide Action Bar */
+        ComponentFunctions.hide_action_bar(savedInstanceState, this);
+
+        on_intent(savedInstanceState);
+    }
+
+    /**
+     * @param bundle Current State Instance
+     *               On Navigate to
+     *               {@link uk.co.johnmelodyme.malaysiansignlanguage.activities.MainActivity}
+     */
+    public void on_intent(Bundle bundle)
+    {
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                ComponentFunctions.route_to(SplashActivity.this, MainActivity.class);
+
+                finish();
+            }
+        }, Constants.SPLASH_SCREEN_TIME_OUT);
     }
 }
--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/WebViewActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/WebViewActivity.java
@@ -6,6 +6,25 @@ import android.os.Bundle;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.R;
 
+/**
+ * @author John Melody Me <johnmelody@dingtalk.com>
+ * <p>
+ * This file is part of Malaysian Sign Language.
+ * <p>
+ * Malaysian Sign Language is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Malaysian Sign Language is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Malaysian Sign Language.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 public class WebViewActivity extends AppCompatActivity
 {
 
