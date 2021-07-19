27cef531b3dcd9dd6adc99dd5bffb4af1a0d110c
John Melody Me <Johnmelodyme@yandex.com>
2021-07-19 23:11:26 +0800

    #1 Added Action Bar items

--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/MalaysianSignLanguageActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/MalaysianSignLanguageActivity.java
@@ -1,11 +1,18 @@
 package uk.co.johnmelodyme.malaysiansignlanguage.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.R;
+import uk.co.johnmelodyme.malaysiansignlanguage.constant.Constants;
+import uk.co.johnmelodyme.malaysiansignlanguage.constant.LogLevel;
 import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
+import uk.co.johnmelodyme.malaysiansignlanguage.functions.Functions;
 
 
 /**
@@ -16,7 +23,7 @@ import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
  * Malaysian Sign Language is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (at THe option) any later version.
  * <p>
  * Malaysian Sign Language is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,9 +36,11 @@ import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
 
 public class MalaysianSignLanguageActivity extends AppCompatActivity
 {
+    public final LogLevel LOG_LEVEL = LogLevel.DEBUG;
+
     public void render_user_components(Bundle bundle)
     {
-
+        Functions.log_output("render_user_components/1", 0, LOG_LEVEL);
     }
 
     @Override
@@ -45,5 +54,78 @@ public class MalaysianSignLanguageActivity extends AppCompatActivity
 
         /* Render Bottom System Bar */
         ComponentFunctions.render_bottom_navigation(this);
+
+        /* Render User Components */
+        render_user_components(savedInstanceState);
+    }
+
+    /**
+     * This hook is called whenever an item in THe options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     *
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.about:
+            {
+                Functions.log_output("menu_about/0", 0, LOG_LEVEL);
+
+                ComponentFunctions.alert_prompt(
+                        getString(R.string.about),
+                        getString(R.string.about_dev),
+                        this
+                );
+
+                break;
+            }
+
+            case R.id.report_issue:
+            {
+                Functions.log_output("menu_report_issue/0", 0, LOG_LEVEL);
+
+                // TODO SMTP Server
+                break;
+            }
+
+            case R.id.forum:
+            {                Functions.log_output("menu_forum/0", 0, LOG_LEVEL);
+
+                Functions.parse_url(Constants.FORUM_URL, this, WebViewActivity.class);
+
+                break;
+
+            }
+
+            default:
+            {
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
     }
 }
--- app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/WebViewActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/malaysiansignlanguage/activities/WebViewActivity.java
@@ -2,9 +2,21 @@ package uk.co.johnmelodyme.malaysiansignlanguage.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import uk.co.johnmelodyme.malaysiansignlanguage.R;
+import uk.co.johnmelodyme.malaysiansignlanguage.constant.LogLevel;
+import uk.co.johnmelodyme.malaysiansignlanguage.functions.ComponentFunctions;
+import uk.co.johnmelodyme.malaysiansignlanguage.functions.Functions;
+
 
 /**
  * @author John Melody Me <johnmelody@dingtalk.com>
@@ -27,11 +39,91 @@ import uk.co.johnmelodyme.malaysiansignlanguage.R;
 
 public class WebViewActivity extends AppCompatActivity
 {
+    public static final LogLevel LOG_LEVEL = LogLevel.DEBUG;
+    public ProgressBar progressBar;
+    public WebView webView;
+
+    /**
+     * @param bundle required for user interface rendering at
+     *               beginning of the Instance.
+     */
+    public void render_user_interface_components(Bundle bundle)
+    {
+        Functions.log_output("render_user_interface_components/1", 0, LOG_LEVEL);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                set_progress_bar_visibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                super.onPageFinished(view, url);
+                set_progress_bar_visibility(View.GONE);
+
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request,
+                                        WebResourceError error)
+            {
+                super.onReceivedError(view, request, error);
+                set_progress_bar_visibility(View.GONE);
+            }
+        });
+
+        /* Render WebView */
+        Functions.open_url_in_app(WebViewActivity.this, bundle, webView);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        /* Render Back Button :: Action Bar */
+        ComponentFunctions.render_back_button_action_bar(this);
+
+        /* Render User Interface Components */
+        render_user_interface_components(savedInstanceState);
+    }
+
+    private void set_progress_bar_visibility(int visibility)
+    {
+        if (progressBar != null)
+        {
+            Functions.log_output("set_progress_bar_visibility/1", 0, LOG_LEVEL);
+
+            progressBar.setVisibility(visibility);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp()
+    {
+        onBackPressed();
+        return true;
+    }
+
+    /**
+     * Called when the activity has detected the user's press of the back
+     * key. The {@link #getOnBackPressedDispatcher() OnBackPressedDispatcher} will be given a
+     * chance to handle the back button before the default behavior of
+     * {@link Activity#onBackPressed()} is invoked.
+     *
+     * @see #getOnBackPressedDispatcher()
+     */
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
     }
 }
